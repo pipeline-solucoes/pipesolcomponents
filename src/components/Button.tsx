fb45c4625987f9735f9ae93263a59c52f9c223d8
Juliana Quaresma <julianaqribeiro@hotmail.com>
2025-01-24 23:23:15 -0300

    Inclusao do styled component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import React from 'react';
+import styled from 'styled-components';
 
 interface ButtonProps {
-  label: string;
-  onClick?: () => void;
+  label: string; 
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => (
-  <button onClick={onClick}>{label}</button>
+const Texto = styled.span`
+  background: #0070f3;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background: #005bb5;
+  }
+`;
+
+const Button: React.FC<ButtonProps> = ({ label }) => (
+  <Texto>{label}</Texto>
 );
 
 export default Button;
